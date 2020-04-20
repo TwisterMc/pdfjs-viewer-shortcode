b90549b2e78ba62cca7c6e52b470f10abf3a7e9c
Thomas McMahon <tmcmahon@nerdery.com>
2020-04-20 16:59:46 -0500

    Starting to add a media upload button

--- blocks/src/block/block.js
+++ blocks/src/block/block.js
@@ -1,10 +1,41 @@
 const { __ } = wp.i18n;
 
+import { addFilter } from '@wordpress/hooks';
+import { Button } from '@wordpress/components';
+const { InspectorControls, InnerBlocks, MediaUpload, MediaUploadCheck } = wp.editor;
+
+const replaceMediaUpload = () => MediaUpload;
+
+const ALLOWED_MEDIA_TYPES = [ 'text' ];
+
+addFilter(
+	'editor.MediaUpload',
+	'core/edit-post/components/media-upload/replace-media-upload',
+	replaceMediaUpload
+);
+
 const { registerBlockType } = wp.blocks;
 
 const {
 	RichText,
 } = wp.editor;
+
+function MyMediaUploader() {
+	return (
+		<MediaUploadCheck>
+			<MediaUpload
+				onSelect={ ( media ) => console.log( 'selected ' + media.length ) }
+				allowedTypes={ ALLOWED_MEDIA_TYPES }
+				value={ mediaId }
+				render={ ( { open } ) => (
+					<Button onClick={ open }>
+						Open Media Library
+					</Button>
+				) }
+			/>
+		</MediaUploadCheck>
+	);
+}
 
 
 registerBlockType(
