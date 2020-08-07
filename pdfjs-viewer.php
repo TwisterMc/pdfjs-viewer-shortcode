9f3f588b992eae5a40af25fda736e116a7d0ff59
Thomas McMahon <tmcmahon@nerdery.com>
2020-08-06 21:41:33 -0500

    Minor Code Cleanup

--- pdfjs-viewer.php
+++ pdfjs-viewer.php
@@ -146,25 +146,28 @@ function my_register_gutenberg_card_block() {
 	}
 
 	// Register our block script with WordPress
-	wp_register_script(
+	wp_register_script (
 		'gutenberg-pdfjs',
-		plugins_url('/blocks/dist/blocks.build.js', __FILE__),
-		array('wp-blocks', 'wp-element', 'wp-editor')
+		plugins_url( '/blocks/dist/blocks.build.js', __FILE__ ),
+		array( 'wp-blocks', 'wp-element', 'wp-editor' )
 	);
 
 	// Register our block's base CSS
-	wp_register_style(
+	wp_register_style (
 		'gutenberg-pdfjs',
 		plugins_url( 'blocks/dist/style.css', __FILE__ ),
 		''
 	);
 
-	register_block_type( 'blocks/pdfjs-block', array(
-		'editor_script' => 'gutenberg-pdfjs',
-		'editor_style'  => 'gutenberg-pdfjs-edit-style',
-		'style'         => 'gutenberg-pdfjs'
-	) );
+	register_block_type(
+		'blocks/pdfjs-block',
+		array(
+			'editor_script' => 'gutenberg-pdfjs',
+			'editor_style'  => 'gutenberg-pdfjs-edit-style',
+			'style'         => 'gutenberg-pdfjs',
+		)
+	);
 
 }
 
-add_action('init', 'my_register_gutenberg_card_block');
+add_action( 'init', 'my_register_gutenberg_card_block' );
