1b60a68c75812e8131952ff2049e5b3d3545b499
Thomas McMahon <tmcmahon@nerdery.com>
2021-03-05 22:36:41 -0600

    updating version number

--- pdfjs-viewer.php
+++ pdfjs-viewer.php
@@ -3,7 +3,7 @@
 Plugin Name: PDFjs Viewer
 Plugin URI: http://byterevel.com/
 Description: Embed PDFs with the gorgeous PDF.js viewer
-Version: 1.5.9
+Version: 2.0.0
 Author: <a href="http://byterevel.com/">Ben Lawson</a>, <a href="https://www.twistermc.com/">Thomas McMahon</a>
 Contributors: FalconerWeb, twistermc
 License: GPLv2
