68b60acd8ebae4f10c9206921abc07d5dede9555
Thomas McMahon <tmcmahon@nerdery.com>
2023-01-05 22:13:28 -0600

    Re-working data sanitization.

--- inc/embed.php
+++ inc/embed.php
@@ -13,17 +13,6 @@ function pdfjs_sanatize_number( $input ) {
 }
 
 /**
- * Takes a setting and ensures it's a number if it's a number or sanitizing it
- */
-function pdfjs_sanatize_text_number( $input ) {
-	if ( is_numeric( $input ) ) {
-		return $input;
-	} else {
-		return sanitize_text_field( $input );
-	}
-}
-
-/**
  * Takes a setting and ensures it returns as true or false
  */
 function pdfjs_set_true_false( $input ) {
@@ -35,23 +24,69 @@ function pdfjs_set_true_false( $input ) {
 }
 
 /**
+ * Checks to see if a string ends with another string
+ */
+function pdfjs_ends_with( $haystack, $needle ) {
+	$length = strlen( $needle );
+	if( !$length ) {
+		return true;
+	}
+	return substr( $haystack, -$length ) === $needle;
+}
+
+/**
+ * Validates pixel and % values
+ */
+function pdfjs_is_percent_or_pixel( $value ) {
+	if ( is_numeric( $value ) ) {
+		return $value;
+	} else if ( pdfjs_ends_with( $value, '%' ) ) {
+		$number = str_replace('%', '', $value);
+		$validNumber = pdfjs_sanatize_number($number);
+		if ($number === $validNumber) {
+			return $value;
+		} else {
+			return '0';
+		}
+	} else if ( pdfjs_ends_with( $value, 'px' ) ) {
+		$number = str_replace('px', '', $value);
+		$validNumber = pdfjs_sanatize_number($number);
+		if ($number === $validNumber) {
+			return $value;
+		} else {
+			return '0';
+		}
+	} else {
+		return '0';
+	}
+}
+
+// check to ensure there are no quotes in the zoom setting so people can't sneak bad stuff in
+function pdfjs_validate_zoom( $zoom ) {
+	if (strpos($zoom, '"') !== FALSE || strpos($zoom, "'") !== FALSE) {
+		return 'auto';
+	}
+	return $zoom;
+}
+
+/**
  * Generate the PDF embed code.
  */
 function pdfjs_generator( $incoming_from_handler ) {
 	$viewer_base_url   = plugin_dir_url( __DIR__ ) . 'pdfjs/web/viewer.php';
-	$viewer_height     = pdfjs_sanatize_text_number( $incoming_from_handler['viewer_height'] );
-	$viewer_width      = pdfjs_sanatize_text_number( $incoming_from_handler['viewer_width'] );
-	$fullscreen        = $incoming_from_handler['fullscreen'];
-	$fullscreen_text   = esc_html( $incoming_from_handler['fullscreen_text'] );
+	$viewer_height     = pdfjs_is_percent_or_pixel( $incoming_from_handler['viewer_height'] );
+	$viewer_width      = pdfjs_is_percent_or_pixel( $incoming_from_handler['viewer_width'] );
+	$fullscreen        = pdfjs_set_true_false( $incoming_from_handler['fullscreen'] );
+	$fullscreen_text   = sanitize_text_field( $incoming_from_handler['fullscreen_text'] );
 	$fullscreen_target = pdfjs_set_true_false( $incoming_from_handler['fullscreen_target'] );
 	$download          = pdfjs_set_true_false( $incoming_from_handler['download'] );
 	$print             = pdfjs_set_true_false( $incoming_from_handler['print'] );
 	$openfile          = pdfjs_set_true_false( $incoming_from_handler['openfile'] );
-	$zoom              = pdfjs_sanatize_text_number( $incoming_from_handler['zoom'] );
+	$zoom              = pdfjs_validate_zoom( $incoming_from_handler['zoom'] );
 	$pagemode          = get_option( 'pdfjs_viewer_pagemode', 'none' );
 	$searchbutton      = get_option( 'pdfjs_search_button', 'on' );
 	$attachment_id     = pdfjs_sanatize_number( $incoming_from_handler['attachment_id'] );
-	$file_url          = esc_url( $incoming_from_handler['url'] );
+	$file_url          = sanitize_url( $incoming_from_handler['url'] );
 	$pdfjs_custom_page = get_option( 'pdfjs_custom_page', '' );
 
 	set_transient( 'pdfjs_button_download_' . $attachment_id, $download );
@@ -70,38 +105,12 @@ function pdfjs_generator( $incoming_from_handler ) {
 		$file_url = esc_url( $file_url );
 	}
 
-	// check to see if the current value is in percent.
-	if ( false === strpos( $viewer_width, '%' ) ) {
-		// check to see if the current value is in pixels.
-		if ( false === strpos( $viewer_width, 'px' ) ) {
-			// check to see if it's 0.
-			if ( '0' === $viewer_width || str_contains($viewer_width, '"') || str_contains($viewer_width, "'")) {
-				$viewer_width = '100%';
-			} else {
-				// add px extension.
-				$viewer_width = $viewer_width . 'px';
-			}
-		}
+	if ( '0' === $viewer_width ) {
+		$viewer_width = '100%';
 	}
 
-	// check to see if the current value is in percent.
-	if ( false === strpos( $viewer_height, '%' ) ) {
-		// check to see if the current value is in pixels.
-		if ( false === strpos( $viewer_height, 'px' ) ) {
-			// check to see if it's 0.
-			if ( '0' === $viewer_height || str_contains($viewer_height, '"') || str_contains($viewer_height, "'")) {
-				$viewer_height = '800';
-			} else {
-				// add px extension.
-				$viewer_height = $viewer_height . 'px';
-			}
-		}
-	}
-
-
-	// check to ensure there are no quotes in the zoom setting so people can't sneak bad stuff in
-	if (str_contains($zoom, '"') || str_contains($zoom, "'")) {
-		$zoom = 'auto';
+	if ( '0' === $viewer_height ) {
+		$viewer_height = '800';
 	}
 
 	if ( 'on' === $searchbutton ) {
@@ -124,9 +133,9 @@ function pdfjs_generator( $incoming_from_handler ) {
 	$fullscreen_link = '';
 	if ( 'true' === $fullscreen ) {
 		if ( $pdfjs_custom_page ) {
-			$fullscreen_link = '<div class="pdfjs-fullscreen"><a href="?pdfjs_id=' . $attachment_id . '&_wpnonce=' . $nonce . '#zoom=' . $zoom . '" ' . $fullscreen_target . '>' . sanitize_text_field( $fullscreen_text ) . '</a></div>';
+			$fullscreen_link = '<div class="pdfjs-fullscreen"><a href="?pdfjs_id=' . $attachment_id . '&_wpnonce=' . $nonce . '#zoom=' . $zoom . '" ' . $fullscreen_target . '>' .  $fullscreen_text  . '</a></div>';
 		} else {
-			$fullscreen_link = '<div class="pdfjs-fullscreen"><a href="' . esc_url( $final_url ) . '" ' . $fullscreen_target . '>' . sanitize_text_field( $fullscreen_text ) . '</a></div>';
+			$fullscreen_link = '<div class="pdfjs-fullscreen"><a href="' . esc_url( $final_url ) . '" ' . $fullscreen_target . '>' .  $fullscreen_text  . '</a></div>';
 		}
 	}
 	$iframe_code = '<div><iframe width="' . $viewer_width . '" height="' . $viewer_height . '" src="' . esc_url( $final_url ) . '" title="Embedded PDF" class="pdfjs-iframe"></iframe></div>';
